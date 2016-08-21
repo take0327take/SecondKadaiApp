a83bb6fcce9cf875538c18596bc47821a8dbab00
竹内健 <TakeshiTakeuchi@takeuchiken-no-macbook-pro.local>
2016-08-21 13:39:02 +0900

    課題提出

---
+++ SecondKadai/NameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NameViewController.swift
+//  SecondKadai
+//
+//  Created by TakeshiTakeuchi on 2016/08/21.
+//  Copyright © 2016年 jp.techacademy.takeshi.takeuchi. All rights reserved.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var name :String! = "name"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view
+        let result = name
+        label.text = "結果は \(result) です"
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
